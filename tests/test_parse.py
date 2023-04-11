81fdb08dd875e7f0e7a9eb592585c8f191c30ef1
Antoni Pokusinski <apokusinski@internships.antmicro.com>
2023-04-11 14:45:46 +0200

    tests: TestHDLParse: use VerilogModule and InterfaceGrouper

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -3,12 +3,18 @@
 
 class TestHDLParse:
     def test_verilog_parse(self):
-        from fpga_topwrap.verilog_parser import ipcore_desc_from_verilog
-        ipcore_desc_from_verilog('tests/data/DMATop.v', None, False)
+        from fpga_topwrap.verilog_parser import ipcore_desc_from_verilog_module, VerilogModule
+        from fpga_topwrap.hdl_parsers_interfaces import InterfaceGrouper
+        verilog_module = VerilogModule('tests/data/DMATop.v')
+        iface_grouper = InterfaceGrouper(True, False, None)
+        ipcore_desc_from_verilog_module(verilog_module, iface_grouper)
 
     def test_verilog_parse_iface_deduce(self):
-        from fpga_topwrap.verilog_parser import ipcore_desc_from_verilog
-        ipcore_desc_from_verilog('tests/data/DMATop.v', None, True)
+        from fpga_topwrap.verilog_parser import ipcore_desc_from_verilog_module, VerilogModule
+        from fpga_topwrap.hdl_parsers_interfaces import InterfaceGrouper
+        verilog_module = VerilogModule('tests/data/DMATop.v')
+        iface_grouper = InterfaceGrouper(False, True, None)
+        ipcore_desc_from_verilog_module(verilog_module, iface_grouper)
 
     def test_vhdl_parse(self):
         from fpga_topwrap.vhdl_parser import ipcore_desc_from_vhdl
