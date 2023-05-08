9a65e72bee7977720ae68ab9addbe18c678706fe
Antoni Pokusinski <apokusinski@internships.antmicro.com>
2023-05-08 13:30:09 +0200

    kpm_topwrap_client: handle import request

--- fpga_topwrap/kpm_topwrap_client.py
+++ fpga_topwrap/kpm_topwrap_client.py
@@ -4,6 +4,8 @@
 import json
 import logging
 from .yamls_to_kpm_spec_parser import ipcore_yamls_to_kpm_spec
+from .design_to_kpm_dataflow_parser import kpm_dataflow_from_design_descr, \
+    design_descr_from_yaml
 from pipeline_manager_backend_communication. \
     communication_backend import CommunicationBackend
 from pipeline_manager_backend_communication \
@@ -16,6 +18,13 @@ def _kpm_specification_handler(yamlfiles: list) -> str:
     """
     specification = ipcore_yamls_to_kpm_spec(yamlfiles)
     return json.dumps(specification)
+
+
+def _kpm_import_handler(data: bytes, yamlfiles: list) -> str:
+    specification = ipcore_yamls_to_kpm_spec(yamlfiles)
+    design_descr = design_descr_from_yaml(data.decode())
+    dataflow = kpm_dataflow_from_design_descr(design_descr, specification)
+    return json.dumps(dataflow)
 
 
 def kpm_run_client(host: str, port: int, yamlfiles: str):
@@ -66,7 +75,8 @@ def kpm_run_client(host: str, port: int, yamlfiles: str):
             elif message_type == MessageType.IMPORT:
                 logging.info(
                     f"Dataflow import request received from {host}:{port}")
+                dataflow = _kpm_import_handler(data, yamlfiles)
                 client.send_message(
-                    MessageType.ERROR,
-                    "Not implemented".encode()
+                    MessageType.OK,
+                    dataflow.encode()
                 )
