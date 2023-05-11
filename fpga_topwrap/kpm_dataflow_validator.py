82e50e911665948f47c23f3521cee98f3f12228a
Antoni Pokusinski <apokusinski@internships.antmicro.com>
2023-05-11 13:02:00 +0200

    kpm_dataflow_validator: add checks for externals

--- fpga_topwrap/kpm_dataflow_validator.py
+++ fpga_topwrap/kpm_dataflow_validator.py
@@ -18,13 +18,65 @@ class CheckResult(NamedTuple):
     message: Union[str, None]
 
 
-def _check_duplicate_ip_names(data: bytes, specification) -> CheckResult:
+class Interface:
+    """ Helper class, representing a KPM node's interface
+    """
+    def __init__(self, node_name: str, iface_name: str, iface_id: str):
+        self.node_name = node_name
+        self.iface_name = iface_name
+        self.iface_id = iface_id
+
+    def __repr__(self) -> str:
+        return self.node_name + ":" + self.iface_name
+
+    def __eq__(self, value: object) -> bool:
+        if isinstance(value, Interface):
+            return value.iface_id == self.iface_id
+        return False
+
+
+def _get_interface_connections(dataflow_data, iface: Interface) -> list:
+    """ Return a list of connections from/to a given interface
+    """
+    return [
+        conn for conn in dataflow_data['graph']['connections'] if conn['from'] == iface.iface_id or conn['to'] == iface.iface_id
+    ]
+
+
+def _get_ips_interfaces(dataflow_data) -> list:
+    """ Return a list of all the interfaces of all the nodes representing ip cores
+    """
+    result = []
+    for node in _get_ip_nodes(dataflow_data):
+        for interface in node['interfaces']:
+            result.append(Interface(node['name'], interface['name'], interface['id']))
+    return result
+
+
+def _get_ip_nodes(dataflow_data) -> list:
+    """ Return a list of nodes which represent ip cores
+    (i.e. filter out External Outputs and Inputs)
+    """
+    return [
+        node for node in dataflow_data['graph']['nodes'] if node['type'] not in ['External Output', 'External Input']
+    ]
+
+def _get_externals_ifaces_ids(dataflow_data) -> list:
+    """ Return a list of ids, which identify metainterfaces of external metanodes
+    """
+    ext_nodes_ifaces_ids = []
+    for node in dataflow_data['graph']['nodes']:
+        if node['type'] in ['External Input', 'External Output']:
+            ext_nodes_ifaces_ids.append(node['interfaces'][0]['id'])
+    return ext_nodes_ifaces_ids
+
+
+def _check_duplicate_ip_names(dataflow_data, specification) -> CheckResult:
     """ Check for duplicate IP core names in the design.
     """
-    dataflow_data = json.loads(data.decode())
     names_set = set()
     duplicates = set()
-    for node in dataflow_data['graph']['nodes']:
+    for node in _get_ip_nodes(dataflow_data):
         if node['name'] in names_set:
             duplicates.add(node['name'])
         else:
@@ -37,11 +89,10 @@ def _check_duplicate_ip_names(data: bytes, specification) -> CheckResult:
     return CheckResult(CheckStatus.ERROR, err_msg)
 
 
-def _check_parameters_values(data: bytes, specification) -> CheckResult:
-    dataflow_data = json.loads(data.decode())
+def _check_parameters_values(dataflow_data, specification) -> CheckResult:
     invalid_params = list()
 
-    for node in dataflow_data['graph']['nodes']:
+    for node in _get_ip_nodes(dataflow_data):
         evaluated = dict()
         for property in node['properties']:
             param_name = property["name"]
@@ -77,9 +128,7 @@ def find_dataflow_interface_by_id(dataflow_json, iface_id: str) -> list | None:
         return ip_interfaces[iface_id]
 
 
-def _check_unconnected_interfaces(data: bytes, specification) -> CheckResult:
-    dataflow_data = json.loads(data.decode())
-
+def _check_unconnected_interfaces(dataflow_data, specification) -> CheckResult:
     unconn_ifaces = set([
         interface['id']
         for node in dataflow_data['graph']['nodes']
@@ -123,12 +172,11 @@ def find_dataflow_node_type_by_name(dataflow_data, node_name: str) -> str:
             return node["type"]
 
 
-def _check_multiple_connections_from_interfaces(data: bytes, specification):
+def _check_multiple_connections_from_interfaces(dataflow_data, specification):
     """ Check for multiple connections going from interfaces.
     A single interfacemay have can be connected with only
     one other interface.
     """
-    dataflow_data = json.loads(data.decode())
     invalid_ifaces = []
 
     for iface in get_dataflow_ips_interfaces(dataflow_data).items():
@@ -156,6 +204,40 @@ def _check_multiple_connections_from_interfaces(data: bytes, specification):
     return CheckResult(CheckStatus.OK, None)
 
 
+def _check_ext_in_to_ext_out_connections(dataflow_data, specification):
+    """ Check for connections between metanodes 'External Input' and 'External Output' metanodes
+    """
+    ext_nodes_ifaces_ids = _get_externals_ifaces_ids(dataflow_data)
+
+    err_conns_ids = []
+    for conn in dataflow_data['graph']['connections']:
+        if conn['from'] in ext_nodes_ifaces_ids and conn['to'] in ext_nodes_ifaces_ids:
+            err_conns_ids.append((conn['from'], conn['id']))
+
+    if not err_conns_ids:
+        return CheckResult(CheckStatus.OK, None)
+    return CheckResult(CheckStatus.ERROR, f"Existing connections from External Inputs to External Outputs")
+
+
+def _check_ambigous_ports_interfaces(dataflow_data, specification):
+    """ Check for interfaces which are connected to another ipcore interface
+    and to external metanode at the same time
+    """
+    ext_nodes_ifaces_ids = _get_externals_ifaces_ids(dataflow_data)
+
+    ambig_ifaces = []
+    for iface in _get_ips_interfaces(dataflow_data):
+        iface_conns = _get_interface_connections(dataflow_data, iface)
+        for conn in iface_conns:
+            if (conn['from'] in ext_nodes_ifaces_ids or conn['to'] in ext_nodes_ifaces_ids) and len(iface_conns) > 1:
+                ambig_ifaces.append(iface)
+                break
+
+    if not ambig_ifaces:
+        return CheckResult(CheckStatus.OK, None)
+    return CheckResult(CheckStatus.ERROR, f"External interfaces having >1 connections: {ambig_ifaces}")
+
+
 def validate_kpm_design(data: bytes, specification) -> dict:
     """ Run some checks to validate user-created design in KPM.
     Return a dict of warning and error messages to be sent to the KPM.
@@ -164,7 +246,9 @@ def validate_kpm_design(data: bytes, specification) -> dict:
         _check_duplicate_ip_names,
         _check_parameters_values,
         _check_unconnected_interfaces,
-        _check_multiple_connections_from_interfaces
+        _check_multiple_connections_from_interfaces,
+        _check_ext_in_to_ext_out_connections,
+        _check_ambigous_ports_interfaces
     ]
 
     messages = {
@@ -172,7 +256,7 @@ def validate_kpm_design(data: bytes, specification) -> dict:
         "warnings": []
     }
     for check in checks:
-        status, msg = check(data, specification)
+        status, msg = check(json.loads(data.decode()), specification)
         if status == CheckStatus.ERROR:
             messages["errors"].append("ERROR: " + msg)
         elif status == CheckStatus.WARNING:
