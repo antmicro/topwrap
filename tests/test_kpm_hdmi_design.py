b7fbb85504fa840f2182290d1626d2e136dc92fc
Antoni Pokusinski <apokusinski@antmicro.com>
2023-06-30 13:30:07 +0200

    [pytest] Add tests for designs export, group design import test

--- tests/test_kpm_hdmi_design.py
+++ tests/test_kpm_hdmi_design.py
@@ -58,19 +58,60 @@ def test_hdmi_specification_generation(hdmi_specification, specification_schema)
     jsonschema.validate(hdmi_specification, specification_schema)
 
 
-def test_hdmi_dataflow_import(hdmi_dataflow):
-    # check number of imported nodes (15 + 8 external metanodes)
-    assert len(hdmi_dataflow['graph']['nodes']) == 23
+class TestHDMIDataflowImport:
+    @pytest.fixture
+    def kpm_nodes(self, hdmi_design_yaml, hdmi_specification):
+        from fpga_topwrap.design_to_kpm_dataflow_parser import kpm_nodes_from_design_descr
+        return [node for node in kpm_nodes_from_design_descr(hdmi_design_yaml, hdmi_specification)]
 
-    # check number of imported externals
-    num_dataflow_externals = len(get_dataflow_metanodes(hdmi_dataflow))
-    assert num_dataflow_externals == 8
+    @pytest.fixture
+    def kpm_metanodes(self, hdmi_design_yaml):
+        from fpga_topwrap.design_to_kpm_dataflow_parser import kpm_metanodes_from_design_descr
+        return [node for node in kpm_metanodes_from_design_descr(hdmi_design_yaml)]
 
-    # check connections between ip cores
-    num_dataflow_ip_conns = len(get_dataflow_ip_connections(hdmi_dataflow))
-    assert num_dataflow_ip_conns == 59
+    def test_hdmi_nodes(self, kpm_nodes):
+        nodes_json = [node.to_json_format() for node in kpm_nodes]
+        assert len(nodes_json) == 15
+        # check overrode {'value': ..., 'width': ...} parameter value
+        axi_interconnect_node = [node for node in nodes_json if node['name'] == 'axi_interconnect0'][0]
+        m_addr_width = [prop for prop in axi_interconnect_node['properties'] if prop['name'] == 'M_ADDR_WIDTH'][0]
+        assert m_addr_width['value'] == '96\'h100000001000000010'
 
-    # check overrode {'value': ..., 'width': ...} parameter value
-    axi_interconnect_node = [node for node in hdmi_dataflow['graph']['nodes'] if node['name'] == 'axi_interconnect0'][0]
-    m_addr_width_prop = [prop for prop in axi_interconnect_node['properties'] if prop['name'] == 'M_ADDR_WIDTH'][0]
-    assert m_addr_width_prop['value'] == '96\'h100000001000000010'
+    def test_hdmi_metanodes(self, kpm_metanodes):
+        metanodes_json = [node.to_json_format() for node in kpm_metanodes]
+        assert len(metanodes_json) == 8
+
+    def test_hdmi_connections(self, hdmi_design_yaml, kpm_nodes):
+        from fpga_topwrap.design_to_kpm_dataflow_parser import kpm_connections_from_design_descr
+        connections = kpm_connections_from_design_descr(hdmi_design_yaml, kpm_nodes)
+        assert len(connections) == 59
+
+    def test_hdmi_metanodes_connections(self, hdmi_design_yaml, kpm_nodes, kpm_metanodes):
+        from fpga_topwrap.design_to_kpm_dataflow_parser import kpm_metanodes_connections_from_design_descr
+        connections = kpm_metanodes_connections_from_design_descr(hdmi_design_yaml, kpm_nodes, kpm_metanodes)
+        assert len(connections) == 8
+
+
+class TestHDMIDataflowExport:
+    def test_parameters(self, hdmi_dataflow):
+        from fpga_topwrap.kpm_dataflow_parser import _kpm_properties_to_parameters
+        axi_node = [node for node in hdmi_dataflow['graph']['nodes'] if node['name'] == 'axi_interconnect0'][0]
+        parameters = _kpm_properties_to_parameters(axi_node['properties'])
+        assert parameters['M_BASE_ADDR'] == int("0x43c2000043c1000043c00000", 16)
+        assert parameters['M_ADDR_WIDTH'] == {'value': int("0x100000001000000010", 16), 'width': 96}
+
+    def test_nodes_to_ips(self, hdmi_design_yaml, hdmi_dataflow, hdmi_ipcores_names_to_yamls):
+        from fpga_topwrap.kpm_dataflow_parser import _kpm_nodes_to_ips
+        ips = _kpm_nodes_to_ips(hdmi_dataflow, hdmi_ipcores_names_to_yamls)
+        assert ips['ips'].keys() == hdmi_design_yaml['ips'].keys()
+
+    def test_port_interfaces(self, hdmi_design_yaml, hdmi_dataflow, hdmi_specification):
+        from fpga_topwrap.kpm_dataflow_parser import _kpm_connections_to_ports_ifaces
+        connections = _kpm_connections_to_ports_ifaces(hdmi_dataflow, hdmi_specification)
+        assert connections['interfaces'] == hdmi_design_yaml['interfaces']
+
+    def test_externals(self, hdmi_design_yaml, hdmi_dataflow):
+        from fpga_topwrap.kpm_dataflow_parser import _kpm_connections_to_external
+        external = _kpm_connections_to_external(hdmi_dataflow)
+        assert external['external']['in'] == {}
+        assert external['external']['out'] == hdmi_design_yaml['external']['out']
--- tests/test_kpm_pwm_design.py
+++ tests/test_kpm_pwm_design.py
@@ -47,19 +47,80 @@ def test_pwm_specification_generation(pwm_specification, specification_schema):
     jsonschema.validate(pwm_specification, specification_schema)
 
 
-def test_pwm_dataflow_import(pwm_dataflow):
-    # check number of imported nodes
-    assert len(pwm_dataflow['graph']['nodes']) == 4
+class TestPWMDataflowImport:
+    @pytest.fixture
+    def kpm_nodes(self, pwm_design_yaml, pwm_specification):
+        from fpga_topwrap.design_to_kpm_dataflow_parser import kpm_nodes_from_design_descr
+        return [node for node in kpm_nodes_from_design_descr(pwm_design_yaml, pwm_specification)]
+    
+    @pytest.fixture
+    def kpm_metanodes(self, pwm_design_yaml):
+        from fpga_topwrap.design_to_kpm_dataflow_parser import kpm_metanodes_from_design_descr
+        return [node for node in kpm_metanodes_from_design_descr(pwm_design_yaml)]
 
-    # check number of imported externals
-    num_dataflow_externals = len(get_dataflow_metanodes(pwm_dataflow))
-    assert num_dataflow_externals == 1
+    def test_pwm_nodes(self, kpm_nodes):
+        nodes_json = [node.to_json_format() for node in kpm_nodes]
+        assert len(nodes_json) == 3
+        ps7_node = [node for node in nodes_json if node['name'] == 'ps7'][0]
+        axi_node = [node for node in nodes_json if node['name']== 'axi_bridge'][0]
+        pwm_node = [node for node in nodes_json if node['name'] == 'litex_pwm_top'][0]
+        # check number of imported properties
+        assert len(ps7_node['properties']) == 0
+        assert len(axi_node['properties']) == 6
+        assert len(pwm_node['properties']) == 0
+        # check overrode parameter value
+        axi_id_width = [prop for prop in axi_node['properties'] if prop['name'] == 'AXI_ID_WIDTH'][0]
+        assert int(axi_id_width['value']) == 12
+        # check number of imported interfaces
+        assert len(ps7_node['interfaces']) == 5
+        assert len(axi_node['interfaces']) == 4
+        assert len(pwm_node['interfaces']) == 4
 
-    # check connections between ip cores
-    num_dataflow_ip_conns = len(get_dataflow_ip_connections(pwm_dataflow))
-    assert num_dataflow_ip_conns == 7
+    def test_pwm_metanodes(self, kpm_metanodes):
+        metanodes_json = [node.to_json_format() for node in kpm_metanodes]
+        assert len(metanodes_json) == 1
+        assert len(metanodes_json[0]['properties']) == 0
+        assert len(metanodes_json[0]['interfaces']) == 1
+        assert metanodes_json[0]['interfaces'][0]['direction'] == 'input'
 
-    # check overrode parameter value
-    axi_node = [node for node in pwm_dataflow['graph']['nodes'] if node['name'] == 'axi_bridge'][0]
-    axi_id_width_prop = [prop for prop in axi_node['properties'] if prop['name'] == 'AXI_ID_WIDTH'][0]
-    assert int(axi_id_width_prop['value']) == 12
+    def test_pwm_connections(self, pwm_design_yaml, kpm_nodes):
+        from fpga_topwrap.design_to_kpm_dataflow_parser import kpm_connections_from_design_descr
+        connections = kpm_connections_from_design_descr(pwm_design_yaml, kpm_nodes)
+        assert len(connections) == 7
+
+    def test_pwm_metanodes_connections(self, pwm_design_yaml, kpm_nodes, kpm_metanodes):
+        from fpga_topwrap.design_to_kpm_dataflow_parser import kpm_metanodes_connections_from_design_descr
+        connections = kpm_metanodes_connections_from_design_descr(pwm_design_yaml, kpm_nodes, kpm_metanodes)
+        assert len(connections) == 1
+
+
+class TestPWMDataflowExport:
+    def test_parameters(self, pwm_dataflow):
+        from fpga_topwrap.kpm_dataflow_parser import _kpm_properties_to_parameters
+        axi_node = [node for node in pwm_dataflow['graph']['nodes'] if node['name'] == 'axi_bridge'][0]
+        parameters = _kpm_properties_to_parameters(axi_node['properties'])
+        assert parameters == {
+            'ADDR_WIDTH': 32,
+            'AXI_DATA_WIDTH': 32,
+            'AXI_ID_WIDTH': 12,
+            'AXI_STRB_WIDTH': 'AXI_DATA_WIDTH/8',
+            'AXIL_DATA_WIDTH': 32,
+            'AXIL_STRB_WIDTH': 'AXIL_DATA_WIDTH/8'
+        }
+
+    def test_nodes_to_ips(self, pwm_design_yaml, pwm_dataflow, pwm_ipcores_names_to_yamls):
+        from fpga_topwrap.kpm_dataflow_parser import _kpm_nodes_to_ips
+        ips = _kpm_nodes_to_ips(pwm_dataflow, pwm_ipcores_names_to_yamls)
+        assert ips['ips'].keys() == pwm_design_yaml['ips'].keys()
+
+    def test_port_interfaces(self, pwm_design_yaml, pwm_dataflow, pwm_specification):
+        from fpga_topwrap.kpm_dataflow_parser import _kpm_connections_to_ports_ifaces
+        connections = _kpm_connections_to_ports_ifaces(pwm_dataflow, pwm_specification)
+        assert connections['ports'] == pwm_design_yaml['ports']
+        assert connections['interfaces'] == pwm_design_yaml['interfaces']
+
+    def test_externals(self, pwm_design_yaml, pwm_dataflow):
+        from fpga_topwrap.kpm_dataflow_parser import _kpm_connections_to_external
+        external = _kpm_connections_to_external(pwm_dataflow)
+        assert external['external']['in'] == {}
+        assert external['external']['out'] == pwm_design_yaml['external']['out']
