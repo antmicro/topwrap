26f338b1d532cd3f821a272f2ca493ed0c7ff5da
Robert Winkler <rwinkler@antmicro.com>
2023-07-18 15:35:14 +0200

    Add hdlConvertor to topwrap dependencies
    Signed-off-by: Robert Winkler <rwinkler@antmicro.com>

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setuptools.setup(
         'amaranth @ git+https://github.com/amaranth-lang/amaranth',
         'wasmtime==1.0.1',
         'numexpr',
+        'hdlConvertor',
         'pipeline_manager_backend_communication @ git+https://github.com/antmicro/kenning-pipeline-manager-backend-communication'
     ],
     setup_requires=["pytest-runner"],
