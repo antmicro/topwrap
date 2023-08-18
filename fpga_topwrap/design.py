c6ffec07f382d0d5a322b0c5f598c2ba7ecd8c14
Antoni Pokusinski <apokusinski@antmicro.com>
2023-08-18 11:44:34 +0200

    design: allow one-level builds
    Internal-tag: [#48074]

--- fpga_topwrap/design.py
+++ fpga_topwrap/design.py
@@ -12,34 +12,44 @@ def build_design_from_yaml(yamlfile, sources_dir=None, part=None):
     build_design(design, sources_dir, part)
 
 
+def generate_design(ips: dict, components: dict, external: dict) -> IPConnect:
+    ipc = IPConnect()
+    ipc_ports = dict()
+    ipc_interfaces = dict()
+
+    for ip_name, ip in components.items():
+        if list(ip.keys()) == ["components", "external"]:
+            hierarchy = generate_design(ips, ip["components"], ip["external"])
+            ipc.add_ip(hierarchy)
+        else:
+            parameters = dict()
+            if "parameters" in ip.keys():
+                parameters = ip["parameters"]
+            if "ports" in ip.keys():
+                ipc_ports[ip_name] = ip["ports"]
+            if "interfaces" in ip.keys():
+                ipc_interfaces[ip_name] = ip["interfaces"]
+            ipc.add_ip(IPWrapper(ips[ip_name]["file"], ips[ip_name]["module"], ip_name, parameters))
+
+    ipc.make_connections(ipc_ports, ipc_interfaces)
+    ipc.make_external_ports_interfaces(ipc_ports, ipc_interfaces, external)
+    return ipc
+
+
 def build_design(design, sources_dir=None, part=None):
-    """Generate a complete project
+    """Build a complete project
 
     :param design: dict describing the top design
     :param sources_dir: directory to scan to include additional HDL files
         to core file
     """
-    ipc = IPConnect()
 
-    ports = dict()
-    interfaces = dict()
+    components = dict()
     external = dict()
-    if "ports" in design.keys():
-        ports = design["ports"]
-    if "interfaces" in design.keys():
-        interfaces = design["interfaces"]
+    if "components" in design.keys():
+        components = design["components"]
     if "external" in design.keys():
         external = design["external"]
 
-    for name, ip in design["ips"].items():
-        if "parameters" not in ip.keys():
-            ip["parameters"] = dict()
-
-        ip_wrapper = IPWrapper(ip["file"], ip["module"], name, ip["parameters"])
-
-        ipc.add_ip(ip_wrapper)
-
-    ipc.make_connections(ports, interfaces)
-
-    ipc.make_external_ports_interfaces(ports, interfaces, external)
+    ipc = generate_design(design["ips"], components, external)
     ipc.build(sources_dir=sources_dir, part=part)
