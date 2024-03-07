c7f35850d91ab532c96243eae8071b79724a5416
Robert Szczepanski <rszczepanski@antmicro.com>
2024-03-07 15:29:00 +0100

    Unify Github and Gitlab CIs
    Internal-tag: [#53659]
    Signed-off-by: Robert Szczepanski <rszczepanski@antmicro.com>

---
+++ .github/scripts/ci.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+# Copyright (c) 2024 Antmicro <www.antmicro.com>
+# SPDX-License-Identifier: Apache-2.0
+
+set -e
+
+ROOT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")"/../.. &>/dev/null && pwd)
+EXAMPLES=(hdmi inout pwm)
+
+begin_command_group() {
+    if [[ -n "${GITHUB_WORKFLOW:-}" ]]; then
+        echo "::group::$*"
+    else
+        echo -e "\n\033[1;92mRunning step: $1\033[0m\n"
+    fi
+}
+
+end_command_group() {
+    if [[ -n "${GITHUB_WORKFLOW:-}" ]]; then
+        echo "::endgroup::"
+    fi
+}
+
+log_cmd() {
+    printf '\033[1;96m'
+    printf '%s ' "$*"
+    printf '\033[0m\n'
+    eval "$*"
+}
+
+install_common_system_packages() {
+    begin_command_group "Install system packages"
+    log_cmd apt-get update -qq
+    log_cmd apt-get install -y --no-install-recommends \
+        git \
+        python3-pip \
+        python3-venv
+    end_command_group
+}
+
+install_pyenv() {
+    begin_command_group "Install pyenv"
+    log_cmd export PYENV_ROOT="$HOME/.pyenv"
+    log_cmd export PATH="$PYENV_ROOT/bin:$PATH"
+    log_cmd "curl https://pyenv.run | bash"
+    end_command_group
+}
+
+enter_venv() {
+    begin_command_group "Configure Python virtual environment"
+    if [[ -z "$VIRTUAL_ENV" ]]; then
+        log_cmd python3 -m venv venv
+    fi
+    log_cmd source venv/bin/activate
+    end_command_group
+}
+
+install_topwrap() {
+    begin_command_group "Install Topwrap"
+    log_cmd "tuttest README.md | bash -"
+    end_command_group
+}
+
+run_lint() {
+    install_common_system_packages
+    enter_venv
+
+    begin_command_group "Install python packages for lint"
+    log_cmd pip install ".[lint]"
+    end_command_group
+
+    begin_command_group "Run lint checks"
+    log_cmd nox -s isort_check black_check flake8
+    end_command_group
+}
+
+run_tests() {
+    install_common_system_packages
+
+    begin_command_group "Install system packages for tests"
+    log_cmd apt-get install -y --no-install-recommends \
+        curl \
+        wget \
+        python3-dev \
+        make \
+        meson \
+        ninja-build \
+        gcc-riscv64-unknown-elf \
+        bsdextrautils \
+        verilator \
+        libssl-dev \
+        libreadline-dev \
+        libffi-dev \
+        libbz2-dev \
+        libncurses-dev \
+        libsqlite3-dev \
+        liblzma-dev
+    end_command_group
+
+    install_pyenv
+    enter_venv
+
+    begin_command_group "Install python packages for tests"
+    log_cmd pip install ".[tests]"
+    log_cmd pip install git+https://github.com/antmicro/tuttest
+    end_command_group
+
+    install_topwrap
+
+    begin_command_group "Run Python tests"
+    log_cmd nox -s tests_in_env
+    end_command_group
+}
+
+generate_examples() {
+    install_common_system_packages
+    begin_command_group "Install system packages for examples"
+    log_cmd apt-get install -y --no-install-recommends python3-dev
+    end_command_group
+    enter_venv
+
+    begin_command_group "Install python packages for examples"
+    log_cmd pip install git+https://github.com/antmicro/tuttest
+    end_command_group
+
+    install_topwrap
+
+    for EXAMPLE in "${EXAMPLES[@]}"; do
+        begin_command_group "Generate $EXAMPLE example"
+        log_cmd pushd "$ROOT_DIR"/examples/"$EXAMPLE"
+        log_cmd "tuttest README.md install-deps,generate | bash -"
+        log_cmd popd
+        end_command_group
+    done
+}
+
+case "$1" in
+lint)
+    run_lint
+    ;;
+tests)
+    run_tests
+    ;;
+examples)
+    generate_examples
+    ;;
+*)
+    echo "Usage: $0 {lint|tests|examples}"
+    ;;
+esac
--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,13 @@
 # Copyright (c) 2023-2024 Antmicro <www.antmicro.com>
 # SPDX-License-Identifier: Apache-2.0
 
+import os
+import shutil
+from pathlib import Path, PurePath
+
 import nox
+
+PYTHON_VERSIONS = ["3.8", "3.9", "3.10", "3.11", "3.12"]
 
 
 @nox.session()
@@ -50,13 +56,53 @@ def black_check(session):
 # https://github.com/pytest-dev/pytest-cov/issues/388
 
 
-@nox.session
+@nox.session(python=PYTHON_VERSIONS)
 def tests(session: nox.Session) -> None:
     session.install("-e", ".[tests,topwrap-parse]")
-    session.run("pytest", "--cov=topwrap", "tests")
+    session.run("pytest", "-rs", "--cov-report", "html:cov_html", "--cov=topwrap", "tests")
+
+
+def prepare_pyenv(session: nox.Session) -> dict:
+    env = os.environ.copy()
+    path = env.get("PATH")
+
+    project_dir = Path(__file__).absolute().parent
+    env["PYENV_ROOT"] = env.get("PYENV_ROOT", f"{project_dir}/.nox/pyenv")
+
+    pyenv_bin = PurePath(env["PYENV_ROOT"]) / "bin"
+    pyenv_shims = PurePath(env["PYENV_ROOT"]) / "shims"
+    path = f"{pyenv_bin}:{pyenv_shims}:{path}"
+    env["PATH"] = path
+
+    # Install Pyenv
+    if not shutil.which("pyenv", path=path):
+        session.error(
+            "\n'pyenv' command not found, you can install it by executing:"
+            "\n    curl https://pyenv.run | bash"
+            "\nSee https://github.com/pyenv/pyenv?tab=readme-ov-file#installation for more information"
+        )
+
+    # Install required Python versions if these don't exist
+    for ver in PYTHON_VERSIONS:
+        if not shutil.which(f"python{ver}", path=path):
+            session.log(f"Installing Python {ver}")
+            session.run("pyenv", "install", ver, external=True, env=env)
+
+    # Detect which versions are provided by Pyenv
+    pythons_in_pyenv = []
+    for ver in PYTHON_VERSIONS:
+        if shutil.which(f"python{ver}", path=pyenv_shims):
+            pythons_in_pyenv += [ver]
+
+    # Allow using Pythons from Pyenv
+    if pythons_in_pyenv:
+        session.log(f"Configuring Pythons from Pyenv, versions: {pythons_in_pyenv}")
+        session.run("pyenv", "global", *pythons_in_pyenv, external=True, env=env)
+
+    return env
 
 
 @nox.session
-def tests_with_report(session: nox.Session) -> None:
-    session.install("-e", ".[tests,topwrap-parse]")
-    session.run("pytest", "--cov-report", "html:cov_html", "--cov=topwrap", "tests")
+def tests_in_env(session: nox.Session) -> None:
+    env = prepare_pyenv(session)
+    session.run("nox", "-s", "tests", external=True, env=env)
