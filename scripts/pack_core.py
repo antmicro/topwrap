22ce47fa4ca2ca991aea7a43258aff1953bcc99d
Rafal Kolucki <rkolucki@antmicro.com>
2024-04-29 13:00:08 +0200

    Update list of `verilog-axi` RTL files, fix logging
    Internal-tag: [#57771]
    Signed-off-by: Rafal Kolucki <rkolucki@antmicro.com>

--- scripts/pack_core.py
+++ scripts/pack_core.py
@@ -20,7 +20,7 @@ click_opt_rw_dir = click.Path(
 click_r_file = click.Path(exists=True, file_okay=True, dir_okay=False, readable=True)
 
 AVAILABLE_LOG_LEVELS = ["NOTSET", "DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
-DEFAULT_LOG_LEVEL = "WARNING"
+DEFAULT_LOG_LEVEL = "INFO"
 
 
 cores = {
@@ -28,52 +28,60 @@ cores = {
         "root": "https://raw.githubusercontent.com/alexforencich/verilog-axi/master/rtl",
         "sources": [
             "arbiter.v",
-            "axi_adapter_rd.v",
             "axi_adapter.v",
+            "axi_adapter_rd.v",
             "axi_adapter_wr.v",
-            "axi_axil_adapter_rd.v",
             "axi_axil_adapter.v",
+            "axi_axil_adapter_rd.v",
             "axi_axil_adapter_wr.v",
+            # "axi_cdma.v",
             "axi_cdma_desc_mux.v",
-            "axi_crossbar_rd.v",
-            "axi_crossbar.v",
-            "axi_crossbar_wr.v",
-            "axi_dma_desc_mux.v",
+            # "axi_crossbar.v",  # unsupported parameter value
+            # "axi_crossbar_addr.v",
+            # "axi_crossbar_rd.v",  # unsupported parameter value
+            # "axi_crossbar_wr.v",  # unsupported parameter value
             "axi_dma.v",
-            "axi_dp_ram.v",
-            "axi_fifo_rd.v",
+            "axi_dma_desc_mux.v",
+            # "axi_dma_rd.v",
+            # "axi_dma_wr.v",
+            # "axi_dp_ram.v", # processing takes too much time
             "axi_fifo.v",
+            "axi_fifo_rd.v",
             "axi_fifo_wr.v",
-            "axil_adapter_rd.v",
-            "axil_adapter.v",
-            "axil_adapter_wr.v",
-            "axil_cdc_rd.v",
-            "axil_cdc.v",
-            "axil_cdc_wr.v",
-            "axil_crossbar_rd.v",
-            "axil_crossbar.v",
-            "axil_crossbar_wr.v",
-            "axil_dp_ram.v",
-            "axil_ram.v",
-            "axil_reg_if_rd.v",
-            "axil_reg_if.v",
-            "axil_reg_if_wr.v",
-            "axil_register_rd.v",
-            "axil_register.v",
-            "axil_register_wr.v",
+            # "axi_interconnect.v",
+            # "axi_ram.v", # processing takes too much time
             "axi_ram_rd_if.v",
-            "axi_ram.v",
             "axi_ram_wr_if.v",
             "axi_ram_wr_rd_if.v",
-            "axi_register_rd.v",
             "axi_register.v",
+            "axi_register_rd.v",
             "axi_register_wr.v",
+            # "axi_vfifo.v",
             "axi_vfifo_dec.v",
-            "axi_vfifo_enc.v",
-            "axi_vfifo_raw_rd.v",
+            # "axi_vfifo_enc.v", # Unsup. expr. on dyn. range sel. on sig. `\seg_mem_rd_data'
             "axi_vfifo_raw.v",
+            "axi_vfifo_raw_rd.v",
             "axi_vfifo_raw_wr.v",
             "axi_vfifo.v",
+            "axil_adapter.v",
+            "axil_adapter_rd.v",
+            "axil_adapter_wr.v",
+            "axil_cdc.v",
+            "axil_cdc_rd.v",
+            "axil_cdc_wr.v",
+            "axil_crossbar.v",
+            # "axil_crossbar_addr.v",
+            "axil_crossbar_rd.v",
+            "axil_crossbar_wr.v",
+            # "axil_dp_ram.v", # processing takes too much time
+            # "axil_interconnect.v",
+            # "axil_ram.v", # processing takes too much time
+            "axil_reg_if.v",
+            "axil_reg_if_rd.v",
+            "axil_reg_if_wr.v",
+            "axil_register.v",
+            "axil_register_rd.v",
+            "axil_register_wr.v",
             "priority_encoder.v",
         ],
     }
@@ -121,6 +129,7 @@ def pack_core(log_level: str, name: str):
     core_files = []
 
     for file in core["sources"]:
+        logging.info(f"Fetching: {file}")
         core_files.append(HttpGetFile(f"{root_path}/{file}"))
 
     core_repo.add_files(VerilogFileHandler(core_files))
