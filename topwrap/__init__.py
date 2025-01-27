4ccfadbf073da4f5419913cbce561298d243346f
Mikołaj Szałkowski <mszalkowski@internships.antmicro.com>
2025-01-27 16:13:23 +0100

    Pin amaranth-yosys and use it by default

--- topwrap/__init__.py
+++ topwrap/__init__.py
@@ -1,2 +1,7 @@
 # Copyright (c) 2021-2024 Antmicro <www.antmicro.com>
 # SPDX-License-Identifier: Apache-2.0
+
+import os
+
+if (var := "AMARANTH_USE_YOSYS") not in os.environ:
+    os.environ[var] = "builtin"
